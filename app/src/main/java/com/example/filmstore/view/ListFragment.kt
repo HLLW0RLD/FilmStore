d1f727a22ac8417f93e579083d75e1d4295cbe54
HLLW0RLD <hllwrld1006@gmail.com>
2021-08-05 17:25:11 +0300

    добавил обработку данных

--- app/src/main/java/com/example/filmstore/view/ListFragment.kt
+++ app/src/main/java/com/example/filmstore/view/ListFragment.kt
@@ -5,7 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.filmstore.R
+import com.example.filmstore.databinding.FragmentListBinding
+import com.example.filmstore.model.AppState
 import com.example.filmstore.viewmodel.MainViewModel
 
 class ListFragment : Fragment() {
@@ -14,17 +20,41 @@ class ListFragment : Fragment() {
         fun newInstance() = ListFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
+    private var _binding: FragmentListBinding? = null
+    private val binding
+                get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_list, container, false)
+    ): View {
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        val observer = Observer<AppState>{renderData(it)}
+        viewModel.getData().observe(viewLifecycleOwner, observer)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun renderData(data: AppState) {
+
+        when(data) {
+            is AppState.Success -> {
+                val filmData = data.filmData
+                binding.loadingLayout.visability = view.GONE
+            }
+        }
     }
 }
--- app/src/main/java/com/example/filmstore/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/filmstore/viewmodel/MainViewModel.kt
@@ -1,9 +1,17 @@
 package com.example.filmstore.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.filmstore.model.AppState
 import com.example.filmstore.model.repository.Repository
 import com.example.filmstore.model.repository.RepositoryImpl
 
 class MainViewModel(private val repository: Repository = RepositoryImpl()) :
     ViewModel() {
+
+    private val liveDataToObserve: MutableLiveData<AppState> = MutableLiveData()
+
+    fun getData(): LiveData<AppState> = liveDataToObserve
+
 }
